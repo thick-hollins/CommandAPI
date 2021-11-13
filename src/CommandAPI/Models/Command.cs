2b8d2cf54cbd9bbf84c9378e202e80e7cf354384
thick-hollins <alexnuttall@zoho.eu>
2021-11-13 21:13:33 +0000

    add command model

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandApi.Models;
+
+public class Command
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(250)]
+    public string HowTo { get; set; } = null!;
+
+    [Required]
+    public string? Platform { get; set; } = null!;
+
+    [Required]
+    public string? CommandLine { get; set; } = null!;
+}
